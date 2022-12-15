d21456d046a28caf380444f86951baede2bae57b
Ixchel García <ixcheldelsun@gmail.com>
2022-12-14 22:14:20 -0400

    feat: :sparkles: only specific reasons for banning

--- app/apps/bans/models.py
+++ app/apps/bans/models.py
@@ -3,7 +3,14 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Ban(models.Model):
-    reason = models.CharField(_("razón del ban"), max_length=150)
+    
+    class Reason(models.TextChoices):
+        LANGUAGE = 'LANGUAGE', _('Foul language')
+        CHEATING = 'CHEATING', _('Cheating')
+        HARRASMENT = 'HARRASMENT', _('Harrasment')
+        OTHER = 'OTHER', _('Other')
+        
+    reason = models.CharField(_("razón del ban"), choices=Reason.choices, default=Reason.CHEATING, max_length=150)
     date = models.DateTimeField(_("fecha del ban"), auto_now=True)
     game = models.ForeignKey('games.Game', on_delete=models.RESTRICT, related_name='game_bans')
     player = models.ForeignKey('players.Player', on_delete=models.RESTRICT, related_name='player_bans')
